83135efcc0fed2f7e841eb4ba793871b7825302d
ShaznilMS <shaznilmussagyoficial@gmail.com>
2024-02-26 14:36:49 -0300

    Update

--- index.js
+++ index.js
@@ -7,7 +7,12 @@ const PORT = 3000;
 // Função para criar rotas
 
 app.get("/", function(req, res) {
-    res.sendFile(__dirname+'/views/index.html');
+    res.sendFile(__dirname+'/views/home.html');
+});app.get("/login", function(req, res) {
+    res.sendFile(__dirname+'/views/login.html');
+});
+app.get("/register", function(req, res) {
+    res.sendFile(__dirname+'/views/register.html');
 });
 
 // Função para criar servidor
