043cf473aa9c5c1f051fa6c1381e09e0b19a3cf9
ShaznilMS <shaznilmussagyoficial@gmail.com>
2024-02-26 14:24:15 -0300

    Update

--- index.js
+++ index.js
@@ -7,19 +7,8 @@ const PORT = 3000;
 // Função para criar rotas
 
 app.get("/", function(req, res) {
-    res.send("Seja bem vindo!");
+    res.sendFile(__dirname+'/views/index.html');
 });
-app.get("/login", function(req, res) {
-    res.send("Página de login");
-});
-app.get("/register", function(req, res) {
-    res.send("Página de registro");
-});
-app.get("/criar/:nome/:profissao", function(req, res) {
-    res.send("<h1 style='font-family: sans-serif;'>Olá " + req.params.nome + "</h1><h2 style='font-family: sans-serif;'>Você é: " + req.params.profissao);
-});
-
-
 
 // Função para criar servidor
 
